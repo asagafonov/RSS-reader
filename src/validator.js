6545fdb071d11093b13852600de33a721464e4ec
asagafonov <a.s.agafonov@yandex.ru>
2020-11-01 20:38:25 +0300

    add border colors based on input validation

--- src/validator.js
+++ src/validator.js
@@ -16,33 +16,49 @@ export default () => {
   const input = document.querySelector('input');
 
   const watchedState = onChange(state, () => {
+    const subline = document.getElementById('subline');
     if (state.inputField === 'invalid') {
+      button.disabled = true;
+      button.setAttribute('aria-disabled', 'true');
       input.classList.add('border');
       input.classList.add('border-danger');
-      button.setAttribute('aria-disabled', 'true');
-      button.disabled = true;
+      input.classList.remove('border-success');
+      subline.textContent = 'Are you sure this is an RSS link?';
+      subline.classList.add('text-danger');
+      subline.classList.remove('text-muted');
+      subline.classList.remove('text-success');
     }
     if (state.inputField === 'valid') {
+      button.disabled = false;
+      button.removeAttribute('aria-disabled');
+      input.classList.add('border-success');
       input.classList.remove('border');
       input.classList.remove('border-danger');
-      button.removeAttribute('aria-disabled');
-      button.disabled = false;
+      subline.textContent = 'Looks like a valid RSS link! Now press add.';
+      subline.classList.add('text-success');
+      subline.classList.remove('text-muted');
+      subline.classList.remove('text-danger');
     }
     if (state.inputField === 'default') {
+      button.disabled = true;
+      button.setAttribute('aria-disabled', 'true');
       input.classList.remove('border');
       input.classList.remove('border-danger');
-      button.setAttribute('aria-disabled', 'true');
-      button.disabled = true;
+      input.classList.remove('border-success');
+      subline.textContent = 'Example: https://ru.hexlet.io/lessons.rss';
+      subline.classList.add('text-muted');
+      subline.classList.remove('text-success');
+      subline.classList.remove('text-danger');
     }
   });
 
   input.addEventListener('input', (e) => {
     watchedState.inputValue = e.target.value;
     const validation = schema.isValidSync(watchedState);
-    if (validation) {
+    if (validation && watchedState.inputValue.match(/rss/)) {
       watchedState.inputField = 'valid';
     }
-    if (!validation) {
+    if (!validation || !watchedState.inputValue.match(/rss/)) {
       watchedState.inputField = 'invalid';
     }
     if (watchedState.inputValue === '') {
