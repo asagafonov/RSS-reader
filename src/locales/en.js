62b5f4161862a693dfd061e77ff425e2176185b9
asagafonov <a.s.agafonov@yandex.ru>
2020-11-04 11:51:08 +0300

    add i18next functionality

---
+++ src/locales/en.js
@@ -0,0 +1,12 @@
+export default {
+  translation: {
+    validation: {
+      warning: 'Are you sure this is an RSS link?',
+      success: 'Looks like a valid RSS link. Now press add.',
+    },
+    alert: {
+      duplication: 'This feed has already been added!',
+      error: 'An error occurred. Please, try again.',
+    },
+  },
+};
