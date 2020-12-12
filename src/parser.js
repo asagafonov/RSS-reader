e26541e66588619a71657333e30921da6a8b5acc
asagafonov <a.s.agafonov@yandex.ru>
2020-12-12 14:34:48 +0300

    upd parser

--- src/parser.js
+++ src/parser.js
@@ -1,4 +1,29 @@
-export default (data, outputFormat) => {
+export default (data, format, url) => {
   const domparser = new DOMParser();
-  return domparser.parseFromString(data, outputFormat);
+  const rss = domparser.parseFromString(data, format);
+
+  const feedTitle = rss.querySelector('channel > title').textContent;
+  const feedDescription = rss.querySelector('channel > description').textContent;
+  const feed = {
+    id: Date.now(),
+    feedTitle,
+    feedDescription,
+    url,
+    posts: [],
+  };
+
+  const items = rss.querySelectorAll('item');
+
+  items.forEach((item) => {
+    const postTitle = item.querySelector('title').textContent;
+    const postLink = item.querySelector('link').textContent;
+    feed.posts.unshift({
+      feedId: feed.id,
+      postId: Date.now(),
+      postTitle,
+      postLink,
+    });
+  });
+
+  return feed;
 };
