aa05b4a6d5218ecb931cb60c90a5349d74e25212
asagafonov <a.s.agafonov@yandex.ru>
2020-12-21 15:39:01 +0300

    add modal buttons & links event

--- src/view.js
+++ src/view.js
@@ -58,62 +58,107 @@ const changeModalWindowContent = (title, description, link) => {
   goToArticleButton.setAttribute('href', link);
 };
 
+const changePostStatus = (posts, link) => {
+  posts.forEach((post) => {
+    if (post.postLink === link) {
+      post.status = 'read'; // eslint-disable-line no-param-reassign
+    }
+  });
+};
+
 const renderFeeds = (state, elements) => {
-  if (state.feeds.length !== 0) {
-    const { feedsContainer } = elements;
-    feedsContainer.innerHTML = '';
-    const feedsTitle = document.createElement('h2');
-    feedsTitle.textContent = i18next.t('pageContent.feeds');
-    feedsContainer.append(feedsTitle);
-    const feedsList = document.createElement('ul');
-    feedsList.setAttribute('class', 'list-group mb-5');
-    feedsContainer.append(feedsList);
-    const { feeds } = state;
-    feeds.forEach((feed) => {
-      const { feedTitle, feedDescription } = feed;
-      const h3 = document.createElement('h3');
-      h3.textContent = feedTitle;
-      const p = document.createElement('p');
-      p.textContent = feedDescription;
+  const { feedsContainer } = elements;
+  feedsContainer.innerHTML = '';
+  const feedsTitle = document.createElement('h2');
+  feedsTitle.textContent = i18next.t('pageContent.feeds');
+  feedsContainer.append(feedsTitle);
+  const feedsList = document.createElement('ul');
+  feedsList.setAttribute('class', 'list-group mb-5');
+  feedsContainer.append(feedsList);
+  const { feeds } = state;
+  feeds.forEach((feed) => {
+    const { feedTitle, feedDescription } = feed;
+    const h3 = document.createElement('h3');
+    h3.textContent = feedTitle;
+    const p = document.createElement('p');
+    p.textContent = feedDescription;
+    const li = document.createElement('li');
+    li.setAttribute('class', 'list-group-item');
+    li.append(h3);
+    li.append(p);
+    feedsList.append(li);
+  });
+
+  const { postsContainer } = elements;
+  postsContainer.innerHTML = '';
+  const postsTitle = document.createElement('h2');
+  postsTitle.textContent = i18next.t('pageContent.posts');
+  postsContainer.append(postsTitle);
+  const postsList = document.createElement('ul');
+  postsList.setAttribute('class', 'list-group');
+  postsContainer.append(postsList);
+  feeds.forEach((feed) => {
+    const { posts } = feed;
+    posts.forEach((post) => {
+      const { postTitle, postLink } = post;
+      const a = document.createElement('a');
+      a.setAttribute('href', postLink);
+      const postsToCheck = state.uiState.posts;
+      postsToCheck.forEach((p) => {
+        if (p.postLink === postLink) {
+          if (p.status === 'read') {
+            a.setAttribute('class', 'font-weight-regular');
+          }
+          if (p.status === 'unread') {
+            a.setAttribute('class', 'font-weight-bold');
+          }
+        }
+      });
+      a.textContent = postTitle;
       const li = document.createElement('li');
-      li.setAttribute('class', 'list-group-item');
-      li.append(h3);
-      li.append(p);
-      feedsList.append(li);
-    });
-    const { postsContainer } = elements;
-    postsContainer.innerHTML = '';
-    const postsTitle = document.createElement('h2');
-    postsTitle.textContent = i18next.t('pageContent.posts');
-    postsContainer.append(postsTitle);
-    const postsList = document.createElement('ul');
-    postsList.setAttribute('class', 'list-group');
-    postsContainer.append(postsList);
-    feeds.forEach((feed) => {
-      const { posts } = feed;
-      posts.forEach((post) => {
-        const { postTitle, postLink, status } = post;
-        const a = document.createElement('a');
-        a.setAttribute('href', postLink);
-        a.setAttribute('class', 'font-weight-bold');
-        a.textContent = postTitle;
-        const li = document.createElement('li');
-        li.setAttribute('class', 'list-group-item d-flex justify-content-between align-items-start');
-        li.append(a);
+      li.setAttribute('class', 'list-group-item d-flex justify-content-between align-items-start');
+      li.append(a);
 
-        const button = document.createElement('button');
-        button.setAttribute('type', 'button');
-        button.setAttribute('class', 'btn btn-primary btn-sm');
-        button.setAttribute('data-toggle', 'modal');
-        button.setAttribute('data-target', '#modal');
-        button.setAttribute('id', postLink);
-        button.textContent = i18next.t('pageContent.previewButton');
-        li.append(button);
+      const button = document.createElement('button');
+      button.setAttribute('type', 'button');
+      button.setAttribute('class', 'btn btn-primary btn-sm');
+      button.setAttribute('data-toggle', 'modal');
+      button.setAttribute('data-target', '#modal');
+      button.setAttribute('id', postLink);
+      button.textContent = i18next.t('pageContent.previewButton');
+      li.append(button);
 
-        postsList.append(li);
+      postsList.append(li);
+
+      const modalButtons = document.querySelectorAll('button[data-toggle="modal"]');
+
+      modalButtons.forEach((btn) => {
+        btn.addEventListener('click', (event) => {
+          const link = event.target.id;
+          state.feeds.forEach((currFeed) => {
+            currFeed.posts.forEach((currPost) => {
+              if (currPost.postLink === link) {
+                const { postTitle: title, postDescription } = currPost;
+                changeModalWindowContent(title, postDescription, link);
+                changePostStatus(postsToCheck, link);
+                renderFeeds(state, elements);
+              }
+            });
+          });
+        });
+      });
+
+      const hrefs = document.querySelectorAll('li > a');
+
+      hrefs.forEach((text) => {
+        text.addEventListener('click', (e) => {
+          const link = e.target.href;
+          changePostStatus(postsToCheck, link);
+          renderFeeds(state, elements);
+        });
       });
     });
-  }
+  });
 };
 
 const initView = (state, elements) => {
@@ -166,8 +211,6 @@ const initView = (state, elements) => {
 };
 
 export {
-  renderFeeds,
   initView,
   buildModalWindow,
-  changeModalWindowContent,
 };
