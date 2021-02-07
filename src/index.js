3556ec5eea7c13aea1d0c8785340e13980d1b88b
asagafonov <a.s.agafonov@yandex.ru>
2021-02-07 14:10:04 +0300

    disable rule for dynamic imports

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-unused-expressions */
+import app from './app.js';
+
 import('bootstrap/dist/css/bootstrap.min.css');
 import('bootstrap/js/dist/modal');
-import app from './app.js';
 
 app();
